99314d612c3d9eea5467bf5d5bc70f59e6c7ac61
Cendekia Astian <cendiastian@gmail.com>
2022-01-06 18:43:22 +0700

    Calling Migration Method

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"ofspace_be/config"
+	"ofspace_be/migration"
 	"ofspace_be/routes"
 )
 
 func main() {
 	config.InitDB()
+	migration.AutoMigrate()
 	e := routes.New()
 	e.Start(":8080")
 }
