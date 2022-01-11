4626bde875569c46238252f6d572705c5a3084bd
RMaha <rizamahardika@mail.ugm.ac.id>
2022-01-11 15:18:40 +0700

    add acc.migration

--- migration/migrate.go
+++ migration/migrate.go
@@ -1,38 +1,23 @@
 package migration
 
+import (
+	"ofspace-be/config"
+	acc "ofspace-be/features/accessibility/data"
+)
+
 func AutoMigrate() {
 	db := config.DB
 	if err := db.Exec("SET FOREIGN_KEY_CHECKS = 0").Error; err != nil {
 		panic(err)
 	}
-	//	pake rds aws sebelumnya dulu aja
-	if err := db.Exec("DROP TABLE IF EXISTS user_course_videos").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS user_courses").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS user_notes").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS user_tasks").Error; err != nil {
-		panic(err)
-	}
+
 	if err := db.Exec("DROP TABLE IF EXISTS users").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS tasks").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS videos").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS courses").Error; err != nil {
 		panic(err)
 	}
 
 	err := db.AutoMigrate(
-	//&user.User{},
+		//&user.User{},
+		&acc.Accessibility{},
 	)
 	if err != nil {
 		panic(err)
