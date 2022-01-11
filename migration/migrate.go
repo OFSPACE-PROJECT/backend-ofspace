35b485c464d4438a6865e9205c0e3f0368b86991
RMaha <rizamahardika@mail.ugm.ac.id>
2022-01-11 14:55:54 +0700

    add migrate

--- migration/migrate.go
+++ migration/migrate.go
@@ -1,38 +1,27 @@
 package migration
 
+import (
+	"ofspace-be/config"
+	complex2 "ofspace-be/features/complex/data"
+)
+
 func AutoMigrate() {
 	db := config.DB
 	if err := db.Exec("SET FOREIGN_KEY_CHECKS = 0").Error; err != nil {
 		panic(err)
 	}
-	//	pake rds aws sebelumnya dulu aja
-	if err := db.Exec("DROP TABLE IF EXISTS user_course_videos").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS user_courses").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS user_notes").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS user_tasks").Error; err != nil {
-		panic(err)
-	}
+
 	if err := db.Exec("DROP TABLE IF EXISTS users").Error; err != nil {
 		panic(err)
 	}
-	if err := db.Exec("DROP TABLE IF EXISTS tasks").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS videos").Error; err != nil {
-		panic(err)
-	}
-	if err := db.Exec("DROP TABLE IF EXISTS courses").Error; err != nil {
+
+	if err := db.Exec("DROP TABLE IF EXISTS complex").Error; err != nil {
 		panic(err)
 	}
 
 	err := db.AutoMigrate(
-	//&user.User{},
+		//&user.User{},
+		&complex2.Complex{},
 	)
 	if err != nil {
 		panic(err)
