0917d039aa295cb32b942cc639fb939a88ac046e
RMaha <rizamahardika@mail.ugm.ac.id>
2022-01-11 15:22:23 +0700

    add acc.route

--- routes/routes.go
+++ routes/routes.go
@@ -1,8 +1,18 @@
 package routes
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"ofspace-be/factory"
+)
 
 func New() *echo.Echo {
+	presenter := factory.New()
 	e := echo.New()
+	// accessibility
+	e.POST("/accessibility", presenter.AccessibilityPresentation.CreateAccessibility)
+	e.GET("/accessibility/:accessibility_id", presenter.AccessibilityPresentation.GetAccessibility)
+	e.POST("/accessibility/:accessibility_id", presenter.AccessibilityPresentation.RequestAccessibility)
+	e.GET("/accessibility/search", presenter.AccessibilityPresentation.SearchAccessibility)
+	e.PUT("/accessibility/:accessibility_id", presenter.AccessibilityPresentation.UpdateAccessibility)
 	return e
 }
