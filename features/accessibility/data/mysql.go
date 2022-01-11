24cd414b0c0191e9fe7e70cbb6be90fcca948942
RMaha <rizamahardika@mail.ugm.ac.id>
2022-01-11 15:12:00 +0700

    add acc.data

--- features/accessibility/data/mysql.go
+++ features/accessibility/data/mysql.go
@@ -1,1 +1,64 @@
 package data
+
+import (
+	"context"
+	"gorm.io/gorm"
+	accessibility "ofspace-be/features/accessibility"
+)
+
+type accessibilityData struct {
+	Connect *gorm.DB
+}
+
+func NewAccessibilityData(connect *gorm.DB) accessibility.Data {
+	return &accessibilityData{Connect: connect}
+}
+
+func (cd *accessibilityData) CreateAccessibility(ctx context.Context, core accessibility.Core) (accessibility.Core, error) {
+	accessibility1 := fromAccessibilityCore(core)
+	result := cd.Connect.Create(&accessibility1)
+	if result.Error != nil {
+		return accessibility.Core{}, result.Error
+	}
+	return toAccessibilityCore(accessibility1), nil
+}
+
+func (cd *accessibilityData) GetAccessibility(ctx context.Context, id uint) (accessibility.Core, error) {
+	var accessibility1 Accessibility
+	result := cd.Connect.First(&accessibility1, "id= ?", id)
+	if result.Error != nil {
+		return accessibility.Core{}, result.Error
+	}
+	return toAccessibilityCore(accessibility1), nil
+}
+
+func (cd *accessibilityData) SearchAccessibility(ctx context.Context, name string) ([]accessibility.Core, error) {
+	var accessibility1 []Accessibility
+	result := cd.Connect.Where("name LIKE ?", name+"%").Find(&accessibility1)
+	if result.Error != nil {
+		return []accessibility.Core{}, result.Error
+	}
+	return ListToCore(accessibility1), nil
+}
+
+func (cd *accessibilityData) UpdateAccessibility(ctx context.Context, core accessibility.Core) (accessibility.Core, error) {
+	accessibility1 := fromAccessibilityCore(core)
+	result := cd.Connect.Where("id= ?", accessibility1.Id).Updates(&Accessibility{
+		Name:      accessibility1.Name,
+		Longitude: accessibility1.Longitude,
+		Latitude:  accessibility1.Latitude,
+	})
+	if result.Error != nil {
+		return accessibility.Core{}, result.Error
+	}
+	return toAccessibilityCore(accessibility1), nil
+}
+
+func (cd *accessibilityData) RequestAccessibility(ctx context.Context, id uint, name string) (accessibility.Core, error) {
+	var accessibility1 Accessibility
+	result := cd.Connect.Where("id= ? && name= ?", id, name).Create(&accessibility1)
+	if result.Error != nil {
+		return accessibility.Core{}, result.Error
+	}
+	return toAccessibilityCore(accessibility1), nil
+}
--- features/accessibility/data/record.go
+++ features/accessibility/data/record.go
@@ -1,1 +1,44 @@
 package data
+
+import (
+	accessibility "ofspace-be/features/accessibility"
+	"time"
+)
+
+type Accessibility struct {
+	Id        uint `gorm:"primaryKey"`
+	Name      string
+	Longitude string
+	Latitude  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func toAccessibilityCore(c Accessibility) accessibility.Core {
+	return accessibility.Core{
+		Id:        c.Id,
+		Name:      c.Name,
+		Longitude: c.Longitude,
+		Latitude:  c.Latitude,
+		CreatedAt: c.CreatedAt,
+		UpdatedAt: c.UpdatedAt,
+	}
+}
+
+func fromAccessibilityCore(c accessibility.Core) Accessibility {
+	return Accessibility{
+		Id:        c.Id,
+		Name:      c.Name,
+		Longitude: c.Longitude,
+		Latitude:  c.Latitude,
+		CreatedAt: c.CreatedAt,
+		UpdatedAt: c.UpdatedAt,
+	}
+}
+
+func ListToCore(core []Accessibility) (result []accessibility.Core) {
+	for _, complex1 := range core {
+		result = append(result, toAccessibilityCore(complex1))
+	}
+	return
+}
