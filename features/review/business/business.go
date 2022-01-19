3fd5ab9d772b39d471cfc37711e0cdba843b7a4d
Cendekia Astian <cendiastian@gmail.com>
2022-01-20 00:02:00 +0700

    Add Create Review Bussines

--- features/review/business/business.go
+++ features/review/business/business.go
@@ -1,1 +1,31 @@
 package business
+
+import (
+	"ofspace-be/features/review"
+	"time"
+
+	"context"
+)
+
+type ReviewBusiness struct {
+	reviewData     review.Data
+	contextTimeout time.Duration
+}
+
+func NewReviewBusiness(reviewData review.Data, timeout time.Duration) review.Business {
+	return &ReviewBusiness{reviewData: reviewData, contextTimeout: timeout}
+}
+
+func (ub *ReviewBusiness) CreateReview(c context.Context, data review.Core) (review.Core, error) {
+	ctx, error := context.WithTimeout(c, ub.contextTimeout)
+	defer error()
+
+	reviewData, err := ub.reviewData.CreateReview(ctx, data)
+
+	reviewData.CostumerOverallRating = (reviewData.RatingAccess + reviewData.RatingFacility + reviewData.RatingManagement + reviewData.RatingQuality) / 4
+
+	if err != nil {
+		return review.Core{}, err
+	}
+	return reviewData, nil
+}
