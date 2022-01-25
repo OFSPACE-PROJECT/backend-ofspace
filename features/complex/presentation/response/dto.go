88c2664cbd088a3397b28d94f55aa56c39de5574
Cendekia Astian <cendiastian@gmail.com>
2022-01-25 23:08:39 +0700

    update response

--- features/complex/presentation/response/dto.go
+++ features/complex/presentation/response/dto.go
@@ -1,19 +1,24 @@
 package response
 
-import complex2 "ofspace-be/features/complex"
+import (
+	building "ofspace-be/features/building/presentation/response"
+	complex2 "ofspace-be/features/complex"
+)
 
 type Complex struct {
-	Id        uint   `json:"id" form:"id"`
-	Name      string `json:"name" form:"name"`
-	Address   string `json:"address" form:"address"`
-	Latitude  string `json:"latitude" form:"latitude"`
-	Longitude string `json:"longitude" form:"longitude"`
+	Id        uint                `json:"id" form:"id"`
+	Name      string              `json:"name" form:"name"`
+	Buildings []building.Building `json:"buildings" form:"buildings"`
+	Address   string              `json:"address" form:"address"`
+	Latitude  string              `json:"latitude" form:"latitude"`
+	Longitude string              `json:"longitude" form:"longitude"`
 }
 
 func FromComplexCore(c complex2.Core) Complex {
 	return Complex{
 		Id:        c.Id,
 		Name:      c.Name,
+		Buildings: building.ToListBuildingCore(c.Buildings),
 		Address:   c.Address,
 		Latitude:  c.Latitude,
 		Longitude: c.Longitude,
@@ -23,6 +28,7 @@ func ToComplexResponse(c complex2.Core) Complex {
 	return Complex{
 		Id:        c.Id,
 		Name:      c.Name,
+		Buildings: building.ToListBuildingCore(c.Buildings),
 		Address:   c.Address,
 		Longitude: c.Longitude,
 		Latitude:  c.Latitude,
