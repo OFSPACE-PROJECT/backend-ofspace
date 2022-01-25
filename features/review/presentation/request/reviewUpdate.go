5e611e6def8407c4dabc4667a424f112eecc92ad
Cendekia Astian <cendiastian@gmail.com>
2022-01-26 00:18:16 +0700

    add building id in request

--- features/review/presentation/request/reviewUpdate.go
+++ features/review/presentation/request/reviewUpdate.go
@@ -6,6 +6,7 @@ type ReviewUpdate struct {
 	Id               uint    `json:"id"`
 	CustomerId       uint    `json:"customer_id"`
 	UnitId           uint    `json:"unit_id"`
+	BuildingId       uint    `json:"building_id"`
 	BookingId        uint    `json:"booking_id"`
 	RatingAccess     float32 `json:"rating_acces"`
 	RatingFacility   float32 `json:"rating_facility"`
@@ -19,6 +20,7 @@ func (r *ReviewUpdate) ToCoreUpdate() review.Core {
 		Id:               r.Id,
 		CustomerId:       r.CustomerId,
 		UnitId:           r.UnitId,
+		BuildingId:       r.BuildingId,
 		BookingId:        r.BookingId,
 		RatingAccess:     r.RatingAccess,
 		RatingFacility:   r.RatingFacility,
